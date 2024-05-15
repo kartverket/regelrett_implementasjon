4bfaca4ba8739f85fa5dabb4504afab72109cdd6
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-05-15 15:42:17 +0200

    Lagt på Gradle og KTOR-config

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "no.bekk.spire.kartverket"
---
+++ src/main/kotlin/no/bekk/Application.kt
@@ -0,0 +1,12 @@
+package no.bekk
+
+import no.bekk.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
