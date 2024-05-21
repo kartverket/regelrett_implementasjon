1e7da439b041f39bcaf3bc915a6acdb807bd7d68
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-05-21 15:28:35 +0200

    Adding Flyway config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -7,6 +6,12 @@ plugins {
     kotlin("jvm") version "1.9.24"
     id("io.ktor.plugin") version "2.3.11"
     kotlin("plugin.serialization") version "1.5.0"
+    id("org.flywaydb.flyway") version "10.13.0"
+}
+
+flyway {
+    url = "jdbc:postgresql://localhost:5432/kontrollere"
+    locations = arrayOf("filesystem:src/main/resources/db/migration")
 }
 
 group = "no.bekk"
@@ -14,7 +19,6 @@ version = "0.0.1"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
@@ -22,6 +26,13 @@ application {
 repositories {
     mavenCentral()
 }
+
+buildscript {
+    dependencies {
+        classpath("org.flywaydb:flyway-database-postgresql:10.13.0")
+    }
+}
+
 
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
@@ -35,6 +46,9 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml:2.3.11")
     implementation("org.jetbrains.exposed:exposed-core:0.36.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.36.1")
+    val flywayCore = "org.flywaydb:flyway-core:10.13.0"
+    implementation(flywayCore)
+    implementation("org.flywaydb:flyway-database-postgresql:10.13.0")
     implementation("org.postgresql:postgresql:42.2.24")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
