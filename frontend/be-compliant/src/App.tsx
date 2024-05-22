b7cba6b95b301b0d200587dbe845951a2ce59196
Sondre Kanstad <sondrekan@gmail.com>
2024-05-22 09:47:43 +0200

    Fetch data from backend server and render json in frontend

--- frontend/be-compliant/src/App.tsx
+++ frontend/be-compliant/src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+    // State to hold fetched JSON data
+    const [data, setData] = useState<any[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/metodeverk'); // TODO: Place dev url to .env file
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch data: ${response.status}`);
+                }
+
+                const jsonData = await response.json();
+                setData(jsonData["records"]);
+            } catch (error) {
+                setError('Error fetching data');
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {error ? (
+              <div>{error}</div> // Display error if there is any
+          ) : (
+              data.length > 0 ? (
+                  <div> {/* Use ul for semantic HTML */}
+                      {data.map((item, index) => (
+                          <p key={index}>{JSON.stringify(item)}</p> // Display JSON data
+                      ))}
+                  </div>
+              ) : (
+                  "No data to display..." // Display message when there is no data
+              )
+          )}
       </header>
     </div>
   );
