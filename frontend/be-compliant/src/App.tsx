094e8c40621cbb8520c94d144fbb961377effab5
Jakob Helgøy <jakobnhelgoy@gmail.com>
2024-05-27 15:26:53 +0200

    clean up fetchers, set default answer to answer if answered

--- frontend/be-compliant/src/App.tsx
+++ frontend/be-compliant/src/App.tsx
@@ -1,133 +1,110 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import {TableContainer, Table, Thead, Tr, Th, Tbody, Select} from '@kvib/react';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { TableContainer, Table, Thead, Tr, Th, Td, Tbody } from "@kvib/react";
+import { Answer } from "./answer/Answer";
+import { useAnswersFetcher } from "./hooks/answersFetcher";
 
 function App() {
-    const [data, setData] = useState<any[]>([]);
-    const [metadata, setMetadata] = useState<any[]>([]);
-    const [dataError, setDataError] = useState<string | null>(null);
-    const [choices, setChoices] = useState<any[]>([]);
+  const { answers } = useAnswersFetcher();
+  const [data, setData] = useState<any[]>([]);
+  const [metadata, setMetadata] = useState<any[]>([]);
+  const [dataError, setDataError] = useState<string | null>(null);
+  const [choices, setChoices] = useState<any[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:8080/metodeverk'); // TODO: Place dev url to .env file
-                if (!response.ok) {
-                    throw new Error(`Failed to fetch data: ${response.status}`);
-                }
-
-                const jsonData = await response.json();
-                setData(jsonData["metodeverkData"]["records"]);
-                setMetadata(jsonData["metaData"]["tables"]);
-            } catch (error) {
-                setDataError('Error fetching data');
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        if (metadata.length > 0) {
-            const aktivitetsTable = metadata.filter((table: any) => table.id === "tblLZbUqA0XnUgC2v")[0];
-
-            if (aktivitetsTable.length < 0) {
-                throw new Error(`Failed to fetch aktivitetstable`);
-            }
-
-            const optionField = aktivitetsTable.fields.filter((field: any) => field.id === "fldbHk1Ce1Ccw5QvF")[0];
-            const options = optionField.options;
-            setChoices(options.choices)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/metodeverk"); // TODO: Place dev url to .env file
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.status}`);
         }
-    }, [metadata]);
 
-    return (
-        <div>
-            {dataError ? (
-                <div>{dataError}</div> // Display error if there is any
-            ) : (
-                data.length > 0 ? (
-                    <TableContainer>
-                        <Table variant="striped" colorScheme="green" style={{ tableLayout: "auto"}}>
-                            <Thead>
-                                <Tr>
-                                    <Th>ID</Th>
-                                    <Th>Spørsmål</Th>
-                                    <Th>Svaralternativer</Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                {data.map((item, index) => (
-                                    <RecordItem key={index} record={item} choices={choices}/>
-                                ))}
-                            </Tbody>
-                        </Table>
-                    </TableContainer>
-                ) : (
-                    "No data to display..."
-                )
-            )}
-        </div>
-    );
+        const jsonData = await response.json();
+        setData(jsonData["metodeverkData"]["records"]);
+        setMetadata(jsonData["metaData"]["tables"]);
+      } catch (error) {
+        setDataError("Error fetching data");
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (metadata.length > 0) {
+      const aktivitetsTable = metadata.filter(
+        (table: any) => table.id === "tblLZbUqA0XnUgC2v"
+      )[0];
+
+      if (aktivitetsTable.length < 0) {
+        throw new Error(`Failed to fetch aktivitetstable`);
+      }
+
+      const optionField = aktivitetsTable.fields.filter(
+        (field: any) => field.id === "fldbHk1Ce1Ccw5QvF"
+      )[0];
+      const options = optionField.options;
+      const answerOptions = options.choices.map((option: any) => option.name);
+      setChoices(answerOptions);
+    }
+  }, [metadata]);
+
+  return (
+    <div>
+      {dataError ? (
+        <div>{dataError}</div> // Display error if there is any
+      ) : data.length > 0 ? (
+        <TableContainer>
+          <Table
+            variant="striped"
+            colorScheme="green"
+            style={{ tableLayout: "auto" }}
+          >
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Spørsmål</Th>
+                <Th>Svaralternativer</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.map((item, index) => (
+                <RecordItem
+                  key={index}
+                  record={item}
+                  choices={choices}
+                  answer={answers.find(
+                    (answer: any) => answer.questionId === item.fields.ID
+                  )}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      ) : (
+        "No data to display..."
+      )}
+    </div>
+  );
 }
 
 interface RecordItemProps {
-    record: Record<any, any>;
-    choices: any[];
+  record: Record<any, any>;
+  choices: any[];
+  answer: any;
 }
 
-const submitAnswer = async (answer: string, record: Record<any, any>) => {
-    const url = 'http://localhost:8080/answer'; // TODO: Place dev url to .env file
-    const settings = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            actor: "Unknown",
-            questionId: record.fields.ID,
-            question: record.fields.Aktivitiet,
-            answer: answer,
-        })
-    }
-    try {
-        const response = await fetch(url, settings);
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-    } catch (error) {
-        console.error('There was an error with the submitAnswer request:', error);
-    }
-    return
-}
-
-const RecordItem: React.FC<RecordItemProps> = ({record, choices}) => {
-    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        submitAnswer(e.target.value, record)
-    }
-
-    return (
-        <Tr>
-            <td>{record.fields.ID} </td>
-            <td>
-                {record.fields.Aktivitiet}
-            </td>
-            <td>
-                <Select
-                    aria-label="select"
-                    placeholder="Velg alternativ"
-                    onChange={handleChange}
-                >
-                    {choices.map((choice, index) => (
-                        <option value={choice.name} key={index}>
-                            {choice.name}
-                        </option>
-                    ))}
-                </Select>
-            </td>
-        </Tr>
-    );
-}
+const RecordItem: React.FC<RecordItemProps> = ({ record, choices, answer }) => {
+  return (
+    <Tr>
+      <Td>{record.fields.ID} </Td>
+      <Td>{record.fields.Aktivitiet}</Td>
+      <Td>
+        <Answer choices={choices} answer={answer} record={record} />
+      </Td>
+    </Tr>
+  );
+};
 
 export default App;
---
+++ frontend/be-compliant/src/hooks/answersFetcher.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const useAnswersFetcher = () => {
+  const [answers, setAnswers] = useState<any>()
+  const [error, setError] = useState<Error>()
+  useEffect(() => {
+    const fetcher = async () => { 
+      try {
+      const response = await fetch("http://localhost:8080/answers")
+      const answersResponse = await response?.json()
+      setAnswers(answersResponse)
+      } catch (error) {
+        setError(error as Error)
+      }
+    };
+      
+  fetcher();
+  }, [])
+  return { answers, error }
+};
