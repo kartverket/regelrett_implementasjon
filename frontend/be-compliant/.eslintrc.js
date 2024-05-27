73940b1b9d6714808b37b2e389566e770da15db2
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-05-27 10:57:21 +0200

    Added ESlint config

---
+++ frontend/be-compliant/.eslintrc.js
@@ -0,0 +1,14 @@
+// .eslintrc.js
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    rules: {
+    },
+};
