d9cb79db4f2a0680469fb6b86c0c94b5c741a6dd
Sondre Kanstad <sondrekan@gmail.com>
2024-05-28 11:03:42 +0200

    Extract RecordItem to new react component QuestionRow

--- frontend/be-compliant/src/App.tsx
+++ frontend/be-compliant/src/App.tsx
@@ -1,110 +1,92 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import { TableContainer, Table, Thead, Tr, Th, Td, Tbody } from "@kvib/react";
-import { Answer } from "./answer/Answer";
-import { useAnswersFetcher } from "./hooks/answersFetcher";
+import {TableContainer, Table, Thead, Tr, Th, Td, Tbody} from "@kvib/react";
+import {useAnswersFetcher} from "./hooks/answersFetcher";
+import {QuestionRow} from "./questionRow/QuestionRow";
 
 function App() {
-  const { answers } = useAnswersFetcher();
-  const [data, setData] = useState<any[]>([]);
-  const [metadata, setMetadata] = useState<any[]>([]);
-  const [dataError, setDataError] = useState<string | null>(null);
-  const [choices, setChoices] = useState<any[]>([]);
+    const {answers} = useAnswersFetcher();
+    const [data, setData] = useState<any[]>([]);
+    const [metadata, setMetadata] = useState<any[]>([]);
+    const [dataError, setDataError] = useState<string | null>(null);
+    const [choices, setChoices] = useState<any[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:8080/metodeverk"); // TODO: Place dev url to .env file
-        if (!response.ok) {
-          throw new Error(`Failed to fetch data: ${response.status}`);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/metodeverk"); // TODO: Place dev url to .env file
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch data: ${response.status}`);
+                }
+
+                const jsonData = await response.json();
+                setData(jsonData["metodeverkData"]["records"]);
+                setMetadata(jsonData["metaData"]["tables"]);
+            } catch (error) {
+                setDataError("Error fetching data");
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if (metadata.length > 0) {
+            const aktivitetsTable = metadata.filter(
+                (table: any) => table.id === "tblLZbUqA0XnUgC2v"
+            )[0];
+
+            if (aktivitetsTable.length < 0) {
+                throw new Error(`Failed to fetch aktivitetstable`);
+            }
+
+            const optionField = aktivitetsTable.fields.filter(
+                (field: any) => field.id === "fldbHk1Ce1Ccw5QvF"
+            )[0];
+            const options = optionField.options;
+            const answerOptions = options.choices.map((option: any) => option.name);
+            setChoices(answerOptions);
         }
+    }, [metadata]);
 
-        const jsonData = await response.json();
-        setData(jsonData["metodeverkData"]["records"]);
-        setMetadata(jsonData["metaData"]["tables"]);
-      } catch (error) {
-        setDataError("Error fetching data");
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    if (metadata.length > 0) {
-      const aktivitetsTable = metadata.filter(
-        (table: any) => table.id === "tblLZbUqA0XnUgC2v"
-      )[0];
-
-      if (aktivitetsTable.length < 0) {
-        throw new Error(`Failed to fetch aktivitetstable`);
-      }
-
-      const optionField = aktivitetsTable.fields.filter(
-        (field: any) => field.id === "fldbHk1Ce1Ccw5QvF"
-      )[0];
-      const options = optionField.options;
-      const answerOptions = options.choices.map((option: any) => option.name);
-      setChoices(answerOptions);
-    }
-  }, [metadata]);
-
-  return (
-    <div>
-      {dataError ? (
-        <div>{dataError}</div> // Display error if there is any
-      ) : data.length > 0 ? (
-        <TableContainer>
-          <Table
-            variant="striped"
-            colorScheme="green"
-            style={{ tableLayout: "auto" }}
-          >
-            <Thead>
-              <Tr>
-                <Th>ID</Th>
-                <Th>Spørsmål</Th>
-                <Th>Svaralternativer</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {data.map((item, index) => (
-                <RecordItem
-                  key={index}
-                  record={item}
-                  choices={choices}
-                  answer={answers?.find(
-                    (answer: any) => answer.questionId === item.fields.ID
-                  )}
-                />
-              ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      ) : (
-        "No data to display..."
-      )}
-    </div>
-  );
+    return (
+        <div>
+            {dataError ? (
+                <div>{dataError}</div> // Display error if there is any
+            ) : data.length > 0 ? (
+                <TableContainer>
+                    <Table
+                        variant="striped"
+                        colorScheme="green"
+                        style={{tableLayout: "auto"}}
+                    >
+                        <Thead>
+                            <Tr>
+                                <Th>ID</Th>
+                                <Th>Spørsmål</Th>
+                                <Th>Svaralternativer</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {data.map((item, index) => (
+                                <QuestionRow
+                                    key={index}
+                                    record={item}
+                                    choices={choices}
+                                    answer={answers?.find(
+                                        (answer: any) => answer.questionId === item.fields.ID
+                                    )}
+                                />
+                            ))}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            ) : (
+                "No data to display..."
+            )}
+        </div>
+    );
 }
-
-interface RecordItemProps {
-  record: Record<any, any>;
-  choices: any[];
-  answer: any;
-}
-
-const RecordItem: React.FC<RecordItemProps> = ({ record, choices, answer }) => {
-  return (
-    <Tr>
-      <Td>{record.fields.ID} </Td>
-      <Td>{record.fields.Aktivitiet}</Td>
-      <Td>
-        <Answer choices={choices} answer={answer} record={record} />
-      </Td>
-    </Tr>
-  );
-};
 
 export default App;
---
+++ frontend/be-compliant/src/questionRow/QuestionRow.tsx
@@ -0,0 +1,21 @@
+import { Td, Tr } from "@kvib/react";
+import { Answer } from "../answer/Answer";
+
+
+interface QuestionRowProps {
+    record: Record<any, any>;
+    choices: any[];
+    answer: any;
+}
+
+export const QuestionRow = (props: QuestionRowProps) => {
+    return (
+        <Tr>
+            <Td>{props.record.fields.ID} </Td>
+            <Td>{props.record.fields.Aktivitiet}</Td>
+            <Td>
+                <Answer choices={props.choices} answer={props.answer} record={props.record} />
+            </Td>
+        </Tr>
+    );
+};
