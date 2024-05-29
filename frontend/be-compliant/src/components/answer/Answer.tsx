3376e6cc798d77ce0e1a84a8283d4f6170fc6650
Sondre Kanstad <sondrekan@gmail.com>
2024-05-29 16:14:16 +0200

    Fix selected error by using select value instead of select in option

--- frontend/be-compliant/src/components/answer/Answer.tsx
+++ frontend/be-compliant/src/components/answer/Answer.tsx
@@ -1,88 +1,92 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { Select } from "@kvib/react";
+import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import {Select} from "@kvib/react";
 
 export type AnswerType = {
-  questionId: string;
-  answer: string;
-  updated: string;
+    questionId: string;
+    answer: string;
+    updated: string;
 };
 
 interface AnswerProps {
-  choices: string[] | [];
-  answer: AnswerType | undefined;
-  record: Record<string, Fields>;
-  setFetchNewAnswers: Dispatch<SetStateAction<boolean>>;
-  fetchNewAnswers: boolean;
+    choices: string[] | [];
+    answer: AnswerType | undefined;
+    record: Record<string, Fields>;
+    setFetchNewAnswers: Dispatch<SetStateAction<boolean>>;
+    fetchNewAnswers: boolean;
 }
 
 export type Fields = {
-  Kortnavn: string;
-  Pri: string;
-  Løpenummer: number;
-  Ledetid: string;
-  Aktivitiet: string;
-  Område: string;
-  Hvem: string[];
-  Kode: string;
-  ID: string;
+    Kortnavn: string;
+    Pri: string;
+    Løpenummer: number;
+    Ledetid: string;
+    Aktivitiet: string;
+    Område: string;
+    Hvem: string[];
+    Kode: string;
+    ID: string;
 };
 
 export const Answer = (props: AnswerProps) => {
-  const [choices, setChoices] = useState<string[]>(props.choices);
+    const [choices, setChoices] = useState<string[]>(props.choices);
+    const [selectedAnswer, setSelectedAnswer] = useState<string | undefined>(props.answer?.answer);
 
-  useEffect(() => {
-    setChoices(props.choices);
-  }, [props.choices]);
+    useEffect(() => {
+        setChoices(props.choices);
+        setSelectedAnswer(props.answer?.answer);
+    }, [props.choices, props.answer]);
 
-  const submitAnswer = async (
-    answer: string,
-    record: Record<string, Fields>,
-  ) => {
-    const url = "http://localhost:8080/answer"; // TODO: Place dev url to .env file
-    const settings = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        actor: "Unknown",
-        questionId: record.fields.ID,
-        question: record.fields.Aktivitiet,
-        answer: answer,
-        updated: ""
-      }),
+    const submitAnswer = async (
+        answer: string,
+        record: Record<string, Fields>,
+    ) => {
+        const url = "http://localhost:8080/answer"; // TODO: Place dev url to .env file
+        const settings = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                actor: "Unknown",
+                questionId: record.fields.ID,
+                question: record.fields.Aktivitiet,
+                answer: answer,
+                updated: ""
+            }),
+        };
+        try {
+            const response = await fetch(url, settings);
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} ${response.statusText}`);
+            }
+            props.setFetchNewAnswers(true);
+        } catch (error) {
+            console.error("There was an error with the submitAnswer request:", error);
+        }
+        return;
     };
-    try {
-      const response = await fetch(url, settings);
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
-      }
-      props.setFetchNewAnswers(true);
-    } catch (error) {
-      console.error("There was an error with the submitAnswer request:", error);
-    }
-    return;
-  };
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    submitAnswer(e.target.value, props.record);
-  };
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newAnswer:string = e.target.value;
+        setSelectedAnswer(newAnswer);
+        submitAnswer(newAnswer, props.record);
+    };
 
-  return (
-    <Select
-      aria-label="select"
-      placeholder="Velg alternativ"
-      onChange={handleChange}
-    >
-      {choices.map((choice, index) => (
-        <option
-          value={choice}
-          key={index}
-          selected={props.answer?.answer === choice}
+    return (
+        <Select
+            aria-label="select"
+            placeholder="Velg alternativ"
+            onChange={handleChange}
+            value={selectedAnswer}
         >
-          {choice}
-        </option>
-      ))}
-    </Select>
-  );
+            {choices.map((choice, index) => (
+                <option
+                    value={choice}
+                    key={index}
+                >
+                    {choice}
+                </option>
+            ))}
+        </Select>
+    );
 };
