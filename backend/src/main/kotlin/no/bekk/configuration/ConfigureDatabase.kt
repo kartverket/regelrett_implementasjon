c5ccf2fe9abeaf44018a9590a5d284fb5ca373e2
Sondre Kanstad <sondrekan@gmail.com>
2024-06-10 16:11:06 +0200

    WIP: Connect docker with local db, but not working locally

--- backend/src/main/kotlin/no/bekk/configuration/ConfigureDatabase.kt
+++ backend/src/main/kotlin/no/bekk/configuration/ConfigureDatabase.kt
@@ -5,7 +5,7 @@ import java.sql.Connection
 import java.sql.DriverManager
 
 fun getDatabaseConnection(): Connection {
-    val config = ConfigFactory.load()
+    /*val config = ConfigFactory.load()
     val databaseConfig = config.getConfig("ktor.database")
 
     val databaseUrl = databaseConfig.getString("url")
@@ -14,6 +14,18 @@ fun getDatabaseConnection(): Connection {
 
     val url = databaseUrl
     val user = databaseUser
-    val password = databasePassword
-    return DriverManager.getConnection(url, user, password)
+    val password = databasePassword*/
+
+    //val dockerHost = System.getenv("HOST")
+
+    val dbHost = System.getenv("DB_HOST") ?: "localhost"
+    val dbPort = System.getenv("DB_PORT") ?: "5432"
+    val dbName = System.getenv("DB_NAME") ?: "your_db_name"
+    val dbUser = System.getenv("DB_USER") ?: "your_db_user"
+    val dbPassword = System.getenv("DB_PASSWORD") ?: "your_db_password"
+
+    val databaseUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
+
+
+    return DriverManager.getConnection(databaseUrl, dbUser, dbPassword)
 }
