a6900cab02712b614baccc09b55087ebca407168
Jakob Helgøy <jakob@bekk-mac-2812.shared>
2024-06-11 13:34:25 +0200

    change name

--- backend/src/main/kotlin/no/bekk/configuration/ConfigureDatabase.kt
+++ backend/src/main/kotlin/no/bekk/configuration/ConfigureDatabase.kt
@@ -8,7 +8,7 @@ fun getDatabaseConnection(): Connection {
     val dbHost = System.getenv("DB_HOST") ?: "localhost"
     val dbPort = System.getenv("DB_PORT") ?: "5432"
     val dbName = System.getenv("DB_NAME") ?: "kontrollere"
-    val dbUser = System.getenv("DB_USER") ?: ""
+    val dbUser = System.getenv("DB_USER") ?: "insert_name_here_when_running_locally"
     val dbPassword = System.getenv("DB_PASSWORD") ?: "your_db_password"
 
     val databaseUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
