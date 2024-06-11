938131d352e19072c0933c3b1e882301fea9194f
Jakob Helgøy <jakob@bekk-mac-2812.shared>
2024-06-11 09:15:10 +0200

    Change order in docker file

--- backend/src/main/kotlin/no/bekk/configuration/ConfigureDatabase.kt
+++ backend/src/main/kotlin/no/bekk/configuration/ConfigureDatabase.kt
@@ -1,31 +1,17 @@
 package no.bekk.configuration
 
-import com.typesafe.config.ConfigFactory
 import java.sql.Connection
 import java.sql.DriverManager
 
 fun getDatabaseConnection(): Connection {
-    /*val config = ConfigFactory.load()
-    val databaseConfig = config.getConfig("ktor.database")
-
-    val databaseUrl = databaseConfig.getString("url")
-    val databaseUser = databaseConfig.getString("user")
-    val databasePassword = databaseConfig.getString("password")
-
-    val url = databaseUrl
-    val user = databaseUser
-    val password = databasePassword*/
-
-    //val dockerHost = System.getenv("HOST")
 
     val dbHost = System.getenv("DB_HOST") ?: "localhost"
     val dbPort = System.getenv("DB_PORT") ?: "5432"
-    val dbName = System.getenv("DB_NAME") ?: "your_db_name"
-    val dbUser = System.getenv("DB_USER") ?: "your_db_user"
+    val dbName = System.getenv("DB_NAME") ?: "kontrollere"
+    val dbUser = System.getenv("DB_USER") ?: ""
     val dbPassword = System.getenv("DB_PASSWORD") ?: "your_db_password"
 
     val databaseUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
-
 
     return DriverManager.getConnection(databaseUrl, dbUser, dbPassword)
 }
