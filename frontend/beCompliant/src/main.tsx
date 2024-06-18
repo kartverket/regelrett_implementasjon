38d7e932f1acaab2928fc3c61497fa7d4ce38a8c
Jakob Helgøy <jakobnhelgoy@gmail.com>
2024-06-18 15:18:53 +0200

    Fix icons

--- frontend/beCompliant/src/main.tsx
+++ frontend/beCompliant/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Header, KvibProvider } from '@kvib/react';
+import 'material-symbols';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
