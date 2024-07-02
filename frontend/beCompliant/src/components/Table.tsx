46517883aaa93eb8e3d5fceebd56eddeb290f66d
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2024-07-02 14:58:11 +0200

    Use Tanstack table with generic column format

--- frontend/beCompliant/src/components/Table.tsx
+++ frontend/beCompliant/src/components/Table.tsx
@@ -1,7 +1,16 @@
-import { Table, TableContainer, Tbody, Th, Thead, Tr } from '@kvib/react';
+import {
+  CellContext,
+  ColumnDef,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 import { Choice, Field } from '../hooks/datafetcher';
 import { RecordType } from '../pages/TablePage';
-import { QuestionRow } from './questionRow/QuestionRow';
+import { DataTable } from './table/DataTable';
+import { DataTableCell } from './table/DataTableCell';
+import { DataTableHeader } from './table/DataTableHeader';
+import { Question } from './table/Question';
 
 type TableComponentProps = {
   data: RecordType[];
@@ -10,35 +19,24 @@ type TableComponentProps = {
   choices: Choice[];
 };
 
-export function TableComponent({
-  data,
-  fields,
-  choices,
-  team,
-}: TableComponentProps) {
-  return (
-    <TableContainer>
-      <Table variant="striped" colorScheme="gray">
-        <Thead>
-          <Tr>
-            <Th>Når</Th>
-            {fields.map((field) => (
-              <Th key={field.id}>{field.name}</Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.map((item: RecordType) => (
-            <QuestionRow
-              key={item.fields.ID}
-              record={item}
-              choices={choices}
-              tableColumns={fields}
-              team={team}
-            />
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
-  );
+export function TableComponent({ data, fields }: TableComponentProps) {
+  const columns: ColumnDef<any, any>[] = fields.map((field) => ({
+    header: ({ column }) => (
+      <DataTableHeader column={column} header={field.name} />
+    ),
+    accessorKey: 'fields.' + field.name,
+    cell: ({ cell, getValue }: CellContext<any, any>) => (
+      <DataTableCell cell={cell}>
+        <Question value={getValue()} column={field} />
+      </DataTableCell>
+    ),
+  }));
+
+  const table = useReactTable({
+    columns: columns,
+    data: data,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+  return <DataTable table={table} />;
 }
--- frontend/beCompliant/src/components/table/DataTableHeader.tsx
+++ frontend/beCompliant/src/components/table/DataTableHeader.tsx
@@ -1,12 +1,30 @@
-import {TableColumnHeaderProps, Th} from "@kvib/react";
-import {Column} from "@tanstack/react-table";
+import { Button, Icon, TableColumnHeaderProps, Th } from '@kvib/react';
+import { Column } from '@tanstack/react-table';
 
 interface Props<TData, TValue> extends TableColumnHeaderProps {
-    column: Column<TData, TValue>
-    header: string
+  column: Column<TData, TValue>;
+  header: string;
 }
 
-
-export function DataTableHeader<TData, TValue>({column, header, ...rest}: Props<TData, TValue>) {
-    return (<Th key={column.columnDef.id} {...rest}>{header}</Th>)
+export function DataTableHeader<TData, TValue>({
+  column,
+  header,
+  ...rest
+}: Props<TData, TValue>) {
+  return (
+    <Th key={column.columnDef.id} {...rest}>
+      <Button variant="ghost" onClick={column.getToggleSortingHandler()}>
+        {header}
+        {column.getIsSorted() && (
+          <Icon
+            icon={
+              column.getIsSorted() === 'desc'
+                ? 'arrow_drop_down'
+                : 'arrow_drop_up'
+            }
+          />
+        )}
+      </Button>
+    </Th>
+  );
 }
