f799ac42005a05b559f400bd2213eea7b888a7ef
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2024-07-09 12:44:55 +0200

    Add support for answer column in data table

---
+++ frontend/beCompliant/src/components/table/TableCell.tsx
@@ -0,0 +1,63 @@
+import { Box, Tag } from '@kvib/react';
+import { Row } from '@tanstack/react-table';
+import { Choice, Field, RecordType } from '../../types/tableTypes';
+import colorUtils from '../../utils/colorUtils';
+import { AnswerCell } from './AnswerCell';
+
+type QuestionProps = {
+  value: any;
+  column: Field;
+  row: Row<RecordType>;
+  answerable?: boolean;
+};
+
+export const TableCell = ({
+  value,
+  column,
+  row,
+  answerable = false,
+}: QuestionProps) => {
+  if (answerable) {
+    return (
+      <AnswerCell
+        value={value}
+        answerType={column.type}
+        questionId={row.original.fields.ID}
+        questionName={row.original.fields.Aktivitet}
+        comment={row.original.fields.comment ?? ''}
+        choices={column.options?.choices}
+      />
+    );
+  }
+
+  if (value == null) {
+    return <></>;
+  }
+  const backgroundColor = column.options?.choices?.find(
+    (choice: Choice) => choice.name === value
+  )?.color;
+  const backgroundColorHex = colorUtils.getHexForColor(
+    backgroundColor ?? 'grayLight1'
+  );
+  const useWhiteTextColor = colorUtils.shouldUseLightTextOnColor(
+    backgroundColor ?? 'grayLight1'
+  );
+
+  switch (column.type) {
+    case 'multipleSelects':
+      return <Box>{value}</Box>;
+    case 'singleSelect':
+      return (
+        <Tag
+          colorScheme={undefined}
+          backgroundColor={backgroundColorHex ?? 'white'}
+          textColor={useWhiteTextColor ? 'white' : 'black'}
+        >
+          {value}
+        </Tag>
+      );
+    default:
+      console.log(value, column.type);
+  }
+  return <Box>{value}</Box>;
+};
