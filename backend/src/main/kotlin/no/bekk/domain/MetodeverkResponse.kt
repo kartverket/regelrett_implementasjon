c6703be25db6e396886c392bc1f96ec941ac2b5a
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2024-07-10 15:21:26 +0200

    Create extension function of Record to map to internal Question object

--- backend/src/main/kotlin/no/bekk/domain/MetodeverkResponse.kt
+++ backend/src/main/kotlin/no/bekk/domain/MetodeverkResponse.kt
@@ -1,8 +1,14 @@
 package no.bekk.domain
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
+import no.bekk.model.internal.Answer
+import no.bekk.model.internal.Column
+import no.bekk.model.internal.Comment
+import no.bekk.model.internal.Question
+import no.bekk.plugins.DatabaseAnswer
+import no.bekk.plugins.DatabaseComment
 
 @Serializable
 data class AirtableResponse(
@@ -16,3 +22,28 @@ data class Record(
     val createdTime: String,
     val fields: JsonElement
 )
+
+fun Record.mapToQuestion(
+    answer: DatabaseAnswer?,
+    comment: DatabaseComment?
+) = Question(
+        id = id,
+        updated = createdTime,
+        columns = fields.jsonObject.entries.map { (key, value) ->
+            Column(key, value)
+        },
+        answer = Answer(
+            actor = answer?.actor ?: "Ukjent",
+            questionId = id,
+            answer = answer?.Svar ?: "",
+            team = answer?.team,
+            updated = answer?.updated ?: ""
+        ),
+        comment = Comment(
+            actor = comment?.actor ?: "Ukjent",
+            questionId = id,
+            comment = comment?.comment ?: "",
+            team = comment?.team,
+            updated = comment?.updated ?: ""
+        )
+    )
