8aec305649d8369f5407d645db66f4e086f3a40e
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2024-07-11 14:54:04 +0200

    Remove field parameter from Table

--- backend/src/main/kotlin/no/bekk/model/internal/Table.kt
+++ backend/src/main/kotlin/no/bekk/model/internal/Table.kt
@@ -1,12 +1,10 @@
 package no.bekk.model.internal
 
 import kotlinx.serialization.Serializable
-import no.bekk.domain.Field
 
 @Serializable
 data class Table(
     val id: String,
     val name: String,
-    val fields: List<Field>,
     val records: List<Question>,
 )
