ea712f786c854c966993425d499de1a3babae119
André Perzon <andre.perzon@hotmail.com>
2024-07-11 12:43:39 +0200

    Added lokal header

--- frontend/beCompliant/src/routes.tsx
+++ frontend/beCompliant/src/routes.tsx
@@ -6,12 +6,14 @@ import {
 import FrontPage from './pages/FrontPage';
 import { MainTableComponent } from './pages/TablePage';
 import { Header } from '@kvib/react';
+import { Header as Header2 } from './components/Header';
 
 const router = createBrowserRouter([
   {
     element: (
       <>
         <Header logoLinkProps={{ as: ReactRouterLink, isExternal: false }} />
+        <Header2 logoLinkProps={{ as: ReactRouterLink }} />
         <Outlet />
       </>
     ),
