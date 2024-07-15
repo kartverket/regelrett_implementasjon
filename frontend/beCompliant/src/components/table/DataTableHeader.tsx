85ee50516d5f43fc16ebe740fd72d42c79fa407e
André Perzon <andre.perzon@bekk.no>
2024-07-15 10:32:19 +0200

    Update header (#87)
    * Added a menu as the header and inside that menu i put sorting options and hide options
    
    * Added visibility icon
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Anders Grytten Standal <anders@gryttenstandal.no>

--- frontend/beCompliant/src/components/table/DataTableHeader.tsx
+++ frontend/beCompliant/src/components/table/DataTableHeader.tsx
@@ -1,10 +1,15 @@
 import {
   Button,
-  Flex,
   Icon,
-  IconButton,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
   TableColumnHeaderProps,
+  Text,
   Th,
+  useTheme,
 } from '@kvib/react';
 import { Column } from '@tanstack/react-table';
 
@@ -22,6 +27,7 @@ export function DataTableHeader<TData, TValue>({
   setColumnVisibility,
   ...rest
 }: Props<TData, TValue>) {
+  const theme = useTheme();
   const hideColumn = (name: string) => {
     setColumnVisibility((prev) => ({
       ...prev,
@@ -29,36 +35,64 @@ export function DataTableHeader<TData, TValue>({
     }));
   };
 
+  const isSorted = column.getIsSorted() !== false;
+  const isAscending = column.getIsSorted() === 'asc';
+  const isSortedAscending = isSorted && isAscending;
+  const isSortedDescending = isSorted && !isAscending;
+
   return (
     <Th key={column.columnDef.id} {...rest}>
-      <Flex alignItems="center">
-        <IconButton
-          variant="tertiary"
-          size="xs"
-          onClick={() => hideColumn(header)}
-          aria-label={'Remove column from table'}
-          icon={'close'}
-        />
-        <Button variant="ghost" onClick={column.getToggleSortingHandler()}>
-          {header}
-          <Icon
-            aria-label={
-              column.getIsSorted() != false
-                ? column.getIsSorted() === 'desc'
-                  ? 'Sortert synkende'
-                  : 'Sortert stigende'
-                : 'Ikke sortert'
-            }
-            icon={
-              column.getIsSorted() != false
-                ? column.getIsSorted() === 'desc'
-                  ? 'keyboard_arrow_down'
-                  : 'keyboard_arrow_up'
-                : 'unfold_more'
-            }
-          />
-        </Button>
-      </Flex>
+      <Menu gutter={4}>
+        <MenuButton
+          as={Button}
+          variant={'ghost'}
+          iconFill={isSorted}
+          rightIcon={
+            isSortedAscending
+              ? 'arrow_upward'
+              : isSortedDescending
+                ? 'arrow_downward'
+                : 'keyboard_arrow_down'
+          }
+        >
+          <Text fontSize="lg">{header}</Text>
+        </MenuButton>
+        <MenuList>
+          <MenuItem
+            aria-label={'Sorter stigende'}
+            onClick={() => column.toggleSorting(false)}
+            icon={<Icon icon="arrow_upward" />}
+            background={isSortedAscending ? theme.colors.gray[50] : undefined}
+          >
+            <Text fontSize="lg">{'Sorter stigende'}</Text>
+          </MenuItem>
+          <MenuItem
+            aria-label={'Sorter synkende'}
+            onClick={() => column.toggleSorting(true)}
+            icon={<Icon icon="arrow_downward" />}
+            background={isSortedDescending ? theme.colors.gray[50] : undefined}
+          >
+            <Text fontSize="lg">{'Sorter synkende'}</Text>
+          </MenuItem>
+          {isSorted && (
+            <MenuItem
+              aria-label={'Fjern sortering'}
+              onClick={() => column.clearSorting()}
+              icon={<Icon icon="close" />}
+            >
+              <Text fontSize="lg">{'Fjern sortering'}</Text>
+            </MenuItem>
+          )}
+          <MenuDivider />
+          <MenuItem
+            aria-label={'Skjul kolone'}
+            onClick={() => hideColumn(header)}
+            icon={<Icon icon="visibility_off" />}
+          >
+            <Text fontSize="lg">{'Skjul kolonne'}</Text>
+          </MenuItem>
+        </MenuList>
+      </Menu>
     </Th>
   );
 }
