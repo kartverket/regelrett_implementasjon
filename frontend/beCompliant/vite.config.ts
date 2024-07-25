45d970d538d0f9bd9a439c26dfe2af48cf82f19e
larsore <larssorensen98@hotmail.com>
2024-07-25 10:54:10 +0200

    Update vite-config

--- frontend/beCompliant/vite.config.ts
+++ frontend/beCompliant/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  cacheDir: '/tmp/vite_cache',
+  build: {
+    outDir: '/tmp/dist',
+  },
   plugins: [react()],
   server: {
     port: 3000,
     strictPort: true,
     host: true,
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
   },
-})
+});
