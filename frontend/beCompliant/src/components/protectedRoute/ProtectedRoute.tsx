f2c692ee94be7b8ef9516780d7efcd62d466c57f
Sondre Kanstad <sondrekan@gmail.com>
2024-07-29 14:57:03 +0200

    Clean up useAuthCheck hook by moving redirect logic to protectedRoute component

--- frontend/beCompliant/src/components/protectedRoute/ProtectedRoute.tsx
+++ frontend/beCompliant/src/components/protectedRoute/ProtectedRoute.tsx
@@ -1,16 +1,27 @@
 import { useAuthCheck } from '../../hooks/useAuthCheck';
 import { Header } from '@kvib/react';
 import { Link as ReactRouterLink, Outlet } from 'react-router-dom';
+import useBackendUrl from '../../hooks/backendUrl';
+import { useEffect } from 'react';
 
 export const ProtectedRoute = () => {
   const isAuthenticated = useAuthCheck();
-  if (isAuthenticated) {
-    return (
-      <>
-        <Header logoLinkProps={{ as: ReactRouterLink }} />
-        <Outlet />
-      </>
-    );
+  const loginUrl = useBackendUrl('/login');
+
+  useEffect(() => {
+    if (isAuthenticated === false) {
+      window.location.href = loginUrl;
+    }
+  }, [isAuthenticated]);
+
+  if (isAuthenticated === null || !isAuthenticated) {
+    return null;
   }
-  return <></>;
+
+  return (
+    <>
+      <Header logoLinkProps={{ as: ReactRouterLink }} />
+      <Outlet />
+    </>
+  );
 };
--- frontend/beCompliant/src/hooks/useAuthCheck.ts
+++ frontend/beCompliant/src/hooks/useAuthCheck.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { axiosFetch } from '../api/Fetch';
 import useBackendUrl from './backendUrl';
 
@@ -8,10 +8,8 @@ type AuthStatus = {
 };
 
 export const useAuthCheck = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const location = useLocation();
-  const navigate = useNavigate();
-  const loginUrl = useBackendUrl('/login');
   const authStatusUrl = useBackendUrl('/auth-status');
 
   useEffect(() => {
@@ -25,16 +23,14 @@ export const useAuthCheck = () => {
         }
         if (!response.data.authenticated) {
           setIsAuthenticated(false);
-          window.location.href = loginUrl;
         }
       } catch (error) {
         setIsAuthenticated(false);
-        window.location.href = loginUrl;
       }
     };
 
     checkAuthStatus();
-  }, [location, navigate]);
+  }, [location]);
 
   return isAuthenticated;
 };
