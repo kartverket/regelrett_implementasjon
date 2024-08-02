b65567eb9c4d7179180d8f1beb37962d4c2a5c5a
Sondre Kanstad <sondrekan@gmail.com>
2024-08-02 11:00:35 +0200

    Update env fetching to use new function

--- backend/src/main/kotlin/no/bekk/plugins/FlywayMigrate.kt
+++ backend/src/main/kotlin/no/bekk/plugins/FlywayMigrate.kt
@@ -1,14 +1,15 @@
 package no.bekk.plugins
 
 import io.ktor.server.application.*
+import no.bekk.configuration.getEnvVariableOrConfig
 import org.flywaydb.core.Flyway
 
 fun Application.runFlywayMigration() {
-    val dbHost = System.getenv("DB_HOST") ?: environment.config.property("ktor.database.host").getString()
-    val dbPort = System.getenv("DB_PORT") ?: environment.config.property("ktor.database.port").getString()
-    val dbName = System.getenv("DB_NAME") ?: environment.config.property("ktor.database.name").getString()
-    val dbUser = System.getenv("DB_USER") ?: environment.config.property("ktor.database.user").getString()
-    val dbPassword = System.getenv("DB_PASSWORD") ?: environment.config.property("ktor.database.password").getString()
+    val dbHost = getEnvVariableOrConfig("DB_HOST", "ktor.database.host")
+    val dbPort = getEnvVariableOrConfig("DB_PORT", "ktor.database.port")
+    val dbName = getEnvVariableOrConfig("DB_NAME", "ktor.database.name")
+    val dbUser = getEnvVariableOrConfig("DB_USER", "ktor.database.user")
+    val dbPassword = getEnvVariableOrConfig("DB_PASSWORD", "ktor.database.password")
 
     val dbUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
 
