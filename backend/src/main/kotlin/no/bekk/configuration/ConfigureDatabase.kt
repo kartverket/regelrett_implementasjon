a5fef17fb4e3dabc4acd6c4c21a5ff9f45a5a378
Sondre Kanstad <sondrekan@gmail.com>
2024-08-02 10:44:35 +0200

    Add host to db config such that it works on internal docker container

--- backend/src/main/kotlin/no/bekk/configuration/ConfigureDatabase.kt
+++ backend/src/main/kotlin/no/bekk/configuration/ConfigureDatabase.kt
@@ -4,11 +4,12 @@ import java.sql.DriverManager
 import com.typesafe.config.ConfigFactory
 
 fun getDatabaseConnection(): Connection {
+    val dbHost = getEnvVariableOrConfig("DB_HOST", "ktor.database.host")
     val dbName = getEnvVariableOrConfig("DB_NAME", "ktor.database.name")
     val dbUser = getEnvVariableOrConfig("DB_USER", "ktor.database.user")
     val dbPassword = getEnvVariableOrConfig("DB_PASSWORD", "ktor.database.password")
 
-    val databaseUrl = "jdbc:postgresql://localhost:5432/$dbName?currentSchema=regelrett"
+    val databaseUrl = "jdbc:postgresql://$dbHost:5432/$dbName?currentSchema=regelrett"
 
     return DriverManager.getConnection(databaseUrl, dbUser, dbPassword)
 }
