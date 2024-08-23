9e2229cce3be176282537b9e61f262c085e39e19
Sondre Kanstad <sondrekan@gmail.com>
2024-08-23 08:55:13 +0200

    Cast environment variable check to lowercase

--- backend/src/main/kotlin/no/bekk/plugins/Config.kt
+++ backend/src/main/kotlin/no/bekk/plugins/Config.kt
@@ -1,6 +1,8 @@
 package no.bekk.plugins
 
+import io.ktor.util.*
+
 object Config {
     val isDevelopment: Boolean
-        get() = System.getenv("ENVIRONMENT") == "development"
+        get() = System.getenv("ENVIRONMENT").toLowerCasePreservingASCIIRules() == "development"
 }
