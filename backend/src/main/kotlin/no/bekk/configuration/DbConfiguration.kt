7c76195363a358e7521713b5a335701cfcd687bc
larsore <larssorensen98@hotmail.com>
2024-08-29 10:32:09 +0200

    Work fine now

--- backend/src/main/kotlin/no/bekk/configuration/DbConfiguration.kt
+++ backend/src/main/kotlin/no/bekk/configuration/DbConfiguration.kt
@@ -1,7 +1,6 @@
 package no.bekk.configuration
 import java.sql.Connection
 import java.sql.DriverManager
-import com.typesafe.config.ConfigFactory
 
 fun getDatabaseConnection(): Connection {
     val dbConfig = AppConfig.db
--- backend/src/main/kotlin/no/bekk/plugins/FlywayMigrate.kt
+++ backend/src/main/kotlin/no/bekk/plugins/FlywayMigrate.kt
@@ -5,14 +5,12 @@ import org.flywaydb.core.Flyway
 
 fun runFlywayMigration() {
     val flyway = Flyway.configure()
-        .createSchemas(true)
-        .defaultSchema("regelrett")
         .dataSource(
             AppConfig.db.url,
             AppConfig.db.username,
             AppConfig.db.password
         )
-        .locations("filesystem:src/main/resources/db/migration")
+        .schemas("regelrett")
         .load()
     flyway.migrate()
 }
