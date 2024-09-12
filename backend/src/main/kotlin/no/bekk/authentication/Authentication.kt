f55408ed4fea5d21ee499f4af34e8d456cd7f58d
starheim98 <andreas.hernas@bekk.no>
2024-09-12 12:39:06 +0200

    testing

--- backend/src/main/kotlin/no/bekk/authentication/Authentication.kt
+++ backend/src/main/kotlin/no/bekk/authentication/Authentication.kt
@@ -31,7 +31,7 @@ fun Application.installSessions() {
         cookie<UserSession>("user_session") {
             cookie.path = "/"
             cookie.maxAgeInSeconds = 180 * 60
-            cookie.secure
+            cookie.secure = true
             cookie.httpOnly = true
         }
     }
