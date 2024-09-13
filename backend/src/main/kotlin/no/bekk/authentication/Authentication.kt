df238ba8fc2dff213ea8ec1d94e3927895f30fe4
starheim98 <andreas.hernas@bekk.no>
2024-09-13 13:56:19 +0200

    clean up code

--- backend/src/main/kotlin/no/bekk/authentication/Authentication.kt
+++ backend/src/main/kotlin/no/bekk/authentication/Authentication.kt
@@ -32,7 +32,6 @@ fun Application.installSessions() {
             cookie.httpOnly = true
             cookie.extensions["SameSite"] = "None"  // Allow cross-origin cookies
             cookie.secure = true
-            //cookie.domain = "regelrett.bekk.no"
         }
     }
 
