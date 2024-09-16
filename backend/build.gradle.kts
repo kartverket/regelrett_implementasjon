352027dcf575328672fcf763ca044afb3d227dfb
starheim98 <andreas.hernas@bekk.no>
2024-09-16 10:05:03 +0200

    separate application.yaml for gcp deployment

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -16,8 +16,21 @@ version = "0.0.1"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    //val isDevelopment: Boolean = project.ext.has("development")
+    val environment: String = System.getenv("ENV") ?: "local"
+
+    val configFile = if (environment == "gcp") {
+        "application-gcp.yaml"
+    } else {
+        "application.yaml"
+    }
+    
+    applicationDefaultJvmArgs = listOf(
+        //"-Dio.ktor.development=$isDevelopment",
+        //"-Dio.ktor.config=${if (isDevelopment) "application-local.yaml" else "application-gcp.yaml"}",
+        "-Dio.ktor.config=$configFile"
+
+    )
 }
 
 repositories {
