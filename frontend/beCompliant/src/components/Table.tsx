82200dcb5f60862efa6c88b711ce28e3088161e3
Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>
2024-09-17 12:00:27 +0200

    fix timestamp affecting row height

--- frontend/beCompliant/src/components/Table.tsx
+++ frontend/beCompliant/src/components/Table.tsx
@@ -59,7 +59,7 @@ export function TableComponent({ data, tableData }: Props) {
         }
       },
       cell: ({ cell, getValue, row }: CellContext<any, any>) => (
-        <DataTableCell cell={cell}>
+        <DataTableCell cell={cell} padding="2.2rem">
           <TableCell
             value={getValue()}
             column={field}
