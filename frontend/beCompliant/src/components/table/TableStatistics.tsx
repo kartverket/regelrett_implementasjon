230251d9dd7809402f2f83e8c62540cecb618632
Anders Grytten Standal <anders@gryttenstandal.no>
2024-09-26 10:54:18 +0200

    Show circular progress

--- frontend/beCompliant/src/components/table/TableStatistics.tsx
+++ frontend/beCompliant/src/components/table/TableStatistics.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@kvib/react';
+import { CircularProgress, CircularProgressLabel, Text } from '@kvib/react';
 import { Question } from '../../api/types';
 
 interface Props {
@@ -13,17 +13,34 @@ export const TableStatistics = ({ filteredData }: Props) => {
     }
     return count;
   }, 0);
+  const percentageAnswered = Math.round(
+    (numberOfAnswers / numberOfQuestions) * 100
+  );
 
   return (
-    <Text minHeight="28px" fontSize="lg">
-      <Text fontSize="lg" as="b">
-        {numberOfAnswers}
-      </Text>{' '}
-      av{' '}
-      <Text fontSize="lg" as="b">
-        {numberOfQuestions}
-      </Text>{' '}
-      spørsmål besvart
-    </Text>
+    <>
+      <Text minHeight="28px" fontSize="lg">
+        <Text fontSize="lg" as="b">
+          {numberOfAnswers}
+        </Text>{' '}
+        av{' '}
+        <Text fontSize="lg" as="b">
+          {numberOfQuestions}
+        </Text>{' '}
+        spørsmål besvart
+      </Text>
+      <CircularProgress
+        size="150px"
+        value={percentageAnswered}
+        width="fit-content"
+        color="blue.500"
+        trackColor="blue.100"
+        capIsRound={true}
+      >
+        <CircularProgressLabel>
+          <Text fontWeight={700}>{percentageAnswered}%</Text>
+        </CircularProgressLabel>
+      </CircularProgress>
+    </>
   );
 };
