7bcdba6e6bb1b570b2335933466ee91043f18508
André Perzon <andre.perzon@hotmail.com>
2024-09-26 10:32:52 +0200

    Updated the auth-status endpoint to check theexpiry on the  token on the session. renamed the frontend hook that checks the auth-status and removed a duplicate hook

--- frontend/beCompliant/src/components/protectedRoute/ProtectedRoute.tsx
+++ frontend/beCompliant/src/components/protectedRoute/ProtectedRoute.tsx
@@ -1,23 +1,9 @@
-import { useAuthCheck } from '../../hooks/useAuthCheck';
 import { Box, Button, Header, Text } from '@kvib/react';
-import { Link as ReactRouterLink, Outlet } from 'react-router-dom';
-import { useEffect } from 'react';
-import { API_URL_LOGIN } from '../../api/apiConfig';
+import { Outlet, Link as ReactRouterLink } from 'react-router-dom';
 import { useLogOut } from '../../hooks/useLogOut';
 
 export const ProtectedRoute = () => {
-  const isAuthenticated = useAuthCheck();
   const { mutate: logOut } = useLogOut();
-
-  useEffect(() => {
-    if (isAuthenticated === false) {
-      window.location.href = API_URL_LOGIN;
-    }
-  }, [isAuthenticated]);
-
-  if (isAuthenticated === null || !isAuthenticated) {
-    return null;
-  }
 
   return (
     <Box backgroundColor="gray.50" minHeight="100vh">
