6f7772ba29d681f363b703ccc5bb190d3f9dd3f6
Christoffer Bjelke <56649743+chribjel@users.noreply.github.com>
2024-10-08 14:26:35 +0200

    Frisk routes (#330)
    ---------
    
    Co-authored-by: starheim98 <andreas.hernas@bekk.no>

---
+++ backend/src/main/kotlin/no/bekk/routes/FriskRouting.kt
@@ -0,0 +1,38 @@
+package no.bekk.routes
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import no.bekk.authentication.UserSession
+import no.bekk.authentication.hasTeamAccess
+import no.bekk.services.FriskService
+
+
+fun Route.friskRouting() {
+    val friskService = FriskService()
+
+    route("/frisk") {
+        get("/metadata") {
+            val userSession = call.sessions.get<UserSession>() ?: throw IllegalStateException("No Session")
+            val teamId = call.request.queryParameters["teamId"] ?: return@get call.respond(HttpStatusCode.BadRequest)
+
+            if (!hasTeamAccess(call, teamId)) {
+                call.respond(HttpStatusCode.Forbidden)
+                return@get
+            }
+
+            val metadata = friskService.fetchMetadataByTeamId(userSession, teamId)
+            call.respond(metadata)
+        }
+
+        get("/functions/{id}") {
+            val userSession = call.sessions.get<UserSession>() ?: throw IllegalStateException("No Session")
+            val id = call.parameters["id"]?.toIntOrNull() ?: return@get call.respond(HttpStatusCode.BadRequest)
+
+            val function = friskService.fetchFunctionByFunctionId(userSession, id)
+            call.respond(function)
+        }
+    }
+}
