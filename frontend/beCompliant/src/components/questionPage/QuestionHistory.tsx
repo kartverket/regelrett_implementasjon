bb1fbd47d40d10184f5c0e4a93bff5a9a18e6786
Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>
2024-10-16 12:57:25 +0200

    fix

--- frontend/beCompliant/src/components/questionPage/QuestionHistory.tsx
+++ frontend/beCompliant/src/components/questionPage/QuestionHistory.tsx
@@ -36,66 +36,70 @@ export function QuestionHistory({ answers }: Props) {
         </Text>
       </HStack>
       <Divider borderColor="gray.400" marginBottom="4" />
-      <Flex flexDirection="column" gap="3" alignItems="center">
-        <Grid templateColumns="3rem 1fr 1fr 1fr" width="100%">
-          <Text gridColumnStart="2">NÅR</Text>
-          <Text>SVAR</Text>
-          <Text>HVEM</Text>
-        </Grid>
-        <Stepper
-          index={0}
-          orientation="vertical"
-          minHeight="300px"
-          gap="0"
-          width="100%"
-        >
-          {steps.map((answer, index) => (
-            <Step key={index} style={{ width: '100%' }}>
-              <Grid templateColumns="3rem 1fr 1fr 1fr" width="100%">
-                <StepIndicator>
-                  <StepStatus
-                    active={
-                      <Icon
-                        color={theme.colors.green[500]}
-                        icon="circle"
-                        isFilled
-                        size={37}
-                      />
-                    }
-                  />
-                </StepIndicator>
-                <Box>
-                  <StepTitle style={{ fontWeight: 'bold' }}>
-                    Endret svar
-                  </StepTitle>
-                  <StepDescription>
-                    {formatDateTime(answer.updated)}
-                  </StepDescription>
-                </Box>
-                <Stack flexDirection="row" opacity={index == 0 ? 1 : 0.6}>
-                  <Icon icon="trip_origin" color={theme.colors.green[500]} />
-                  <Text color={theme.colors.green[500]}>
-                    {answer.answer}{' '}
-                    {answer.answerType === 'PERCENT'
-                      ? '%'
-                      : answer.answerUnit || ''}
-                  </Text>
-                </Stack>
-                <Stack flexDirection="row" opacity={index == 0 ? 1 : 0.6}>
-                  <Icon
-                    icon="person"
-                    isFilled
-                    color={theme.colors.green[500]}
-                  />
-                  <Text color={theme.colors.green[500]}>{answer.actor}</Text>
-                </Stack>
-                <StepSeparator style={{ justifySelf: 'left' }} />
-              </Grid>
-            </Step>
-          ))}
-        </Stepper>
-        <Divider borderColor="gray.400" marginTop="10" />
-      </Flex>
+      {steps.length === 0 ? (
+        <Text>Ingen historikk finnes</Text>
+      ) : (
+        <Flex flexDirection="column" gap="3" alignItems="center">
+          <Grid templateColumns="3rem 1fr 1fr 1fr" width="100%">
+            <Text gridColumnStart="2">NÅR</Text>
+            <Text>SVAR</Text>
+            <Text>HVEM</Text>
+          </Grid>
+          <Stepper
+            index={0}
+            orientation="vertical"
+            minHeight="300px"
+            gap="0"
+            width="100%"
+          >
+            {steps.map((answer, index) => (
+              <Step key={index} style={{ width: '100%' }}>
+                <Grid templateColumns="3rem 1fr 1fr 1fr" width="100%">
+                  <StepIndicator>
+                    <StepStatus
+                      active={
+                        <Icon
+                          color={theme.colors.green[500]}
+                          icon="circle"
+                          isFilled
+                          size={37}
+                        />
+                      }
+                    />
+                  </StepIndicator>
+                  <Box>
+                    <StepTitle style={{ fontWeight: 'bold' }}>
+                      Endret svar
+                    </StepTitle>
+                    <StepDescription>
+                      {formatDateTime(answer.updated)}
+                    </StepDescription>
+                  </Box>
+                  <Stack flexDirection="row" opacity={index == 0 ? 1 : 0.6}>
+                    <Icon icon="trip_origin" color={theme.colors.green[500]} />
+                    <Text color={theme.colors.green[500]}>
+                      {answer.answer}{' '}
+                      {answer.answerType === 'PERCENT'
+                        ? '%'
+                        : answer.answerUnit || ''}
+                    </Text>
+                  </Stack>
+                  <Stack flexDirection="row" opacity={index == 0 ? 1 : 0.6}>
+                    <Icon
+                      icon="person"
+                      isFilled
+                      color={theme.colors.green[500]}
+                    />
+                    <Text color={theme.colors.green[500]}>{answer.actor}</Text>
+                  </Stack>
+                  <StepSeparator style={{ justifySelf: 'left' }} />
+                </Grid>
+              </Step>
+            ))}
+          </Stepper>
+          <Divider borderColor="gray.400" marginTop="10" />
+        </Flex>
+      )}
     </Flex>
   );
 }
