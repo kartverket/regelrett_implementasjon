deb22fa9fc30dae0fd84c1ecf4af8e2637489587
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-10-17 09:55:19 +0200

    remove all frisk refs

--- backend/src/main/kotlin/no/bekk/Application.kt
+++ backend/src/main/kotlin/no/bekk/Application.kt
@@ -73,12 +73,6 @@ private fun loadAppConfig(config: ApplicationConfig) {
         username = config.propertyOrNull("db.username")?.getString() ?: throw IllegalStateException("Unable to initialize app config \"db.username\"")
         password = config.propertyOrNull("db.password")?.getString() ?: throw IllegalStateException("Unable to initialize app config \"db.password\"")
     }
-
-    AppConfig.FRISK = FRISKConfig.apply {
-        apiUrl = config.propertyOrNull("frisk.apiUrl")?.getString() ?: throw IllegalStateException("Unable to initialize app config \"apiUrl\"")
-        tenantId = config.propertyOrNull("frisk.tenantId")?.getString() ?: throw IllegalStateException("Unable to initialize app config \"frisk.tenantId\"")
-        clientId = config.propertyOrNull("frisk.clientId")?.getString() ?: throw IllegalStateException("Unable to initialize app config \"frisk.clientId\"")
-    }
 }
 
 fun Application.module() {
