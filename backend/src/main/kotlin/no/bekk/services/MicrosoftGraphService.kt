dfc77ff9daa24243f4d109c2b9c70cf3533f2f12
malinnsnieske <malin.snieske@bekk.no>
2024-11-05 12:52:16 +0100

    remove filtering teams by AAD... (#471)
    Co-authored-by: Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>

--- backend/src/main/kotlin/no/bekk/services/MicrosoftGraphService.kt
+++ backend/src/main/kotlin/no/bekk/services/MicrosoftGraphService.kt
@@ -1,7 +1,6 @@
 package no.bekk.services
 
 import com.azure.identity.ClientSecretCredentialBuilder
-import com.microsoft.graph.models.Group
 import com.microsoft.graph.models.User
 import com.microsoft.graph.serviceclient.GraphServiceClient
 import no.bekk.configuration.AppConfig
@@ -19,15 +18,6 @@ object MicrosoftGraphService {
         .clientSecret(clientSecret)
         .build()
     private val graphClient = GraphServiceClient(credential, scopes)
-
-    fun getGroup(groupName: String): Group {
-        return graphClient.groups().get {
-            it.queryParameters.filter = "displayName eq 'AAD - TF - TEAM - $groupName'"
-            it.queryParameters.select = arrayOf("id", "displayName")
-            it.queryParameters.count = true
-            it.headers.add("ConsistencyLevel", "eventual")
-        }.value[0]
-    }
 
     fun getUser(userId: String): User {
         return graphClient.users().byUserId(userId).get()
