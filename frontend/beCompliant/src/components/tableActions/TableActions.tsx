ad56b233a928772ce89095e1a03fa3ae8d8dcc92
malinnsnieske <malin.snieske@bekk.no>
2024-11-06 15:18:55 +0100

    fix top when no back url (#476)
    * fix top when no back url
    ---------
    
    Co-authored-by: Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>

--- frontend/beCompliant/src/components/tableActions/TableActions.tsx
+++ frontend/beCompliant/src/components/tableActions/TableActions.tsx
@@ -2,6 +2,7 @@ import { Flex, Heading, Icon } from '@kvib/react';
 import { Column } from '../../api/types';
 import { TableFilter, TableFilters } from './TableFilter';
 import { ActiveFilter } from '../../types/tableTypes';
+import { useStoredRedirect } from '../../hooks/useStoredRedirect';
 
 interface Props {
   resetTable: () => void;
@@ -16,6 +17,8 @@ export const TableActions = ({
   tableMetadata,
   filterByAnswer,
 }: Props) => {
+  const storedRedirect = useStoredRedirect();
+
   function localSetActiveFilters(activeFilters: ActiveFilter[]) {
     setActiveFilters(activeFilters);
     resetTable();
@@ -28,7 +31,7 @@ export const TableActions = ({
       paddingX="10"
       py="5"
       position="sticky"
-      top="10"
+      top={storedRedirect ? '10' : '0'}
       zIndex="1000"
       backgroundColor="gray.50"
       w="100%"
