b55cbbcb51fed5aa5c19dc1748045a445ba26804
starheim98 <andreas.hernas@bekk.no>
2024-12-09 14:26:22 +0100

    use promise array

--- frontend/beCompliant/src/hooks/useFetchAllContexts.ts
+++ frontend/beCompliant/src/hooks/useFetchAllContexts.ts
@@ -11,14 +11,17 @@ export function useFetchAllContexts() {
   return useQuery({
     queryKey: ['allContexts'],
     queryFn: async () => {
-      const allContexts: Context[] = [];
+      const promises: Promise<Context[]>[] = [];
       for (const team of teams) {
-        const contexts: Context[] = await axiosFetch<Context[]>({
-          url: apiConfig.contexts.forTeam.url(team),
-        }).then((response) => response.data);
-        allContexts.push(...contexts);
+        promises.push(
+          axiosFetch<Context[]>({
+            url: apiConfig.contexts.forTeam.url(team),
+          }).then((response) => response.data)
+        );
       }
-      return allContexts;
+      return (await Promise.all(promises)).reduce((acc, val) => {
+        return acc.concat(val);
+      }, []);
     },
     enabled: !!teams.length,
   });
