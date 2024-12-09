c75b54d10856c308cbb7d9f4be3f76281243d69e
Andreas <60923027+starheim98@users.noreply.github.com>
2024-12-09 14:32:19 +0100

    copy answers from any team (#529)
    * copy answers from any team
    
    * removed comments
    
    * use userinfo cache in fetch all contexts hook
    
    * use promise array

--- frontend/beCompliant/src/components/createContextPage/CopyContextDropdown.tsx
+++ frontend/beCompliant/src/components/createContextPage/CopyContextDropdown.tsx
@@ -1,42 +1,51 @@
-import { Box, FormControl, FormLabel, Select, Skeleton } from '@kvib/react';
-import { useFetchTeamTableContexts } from '../../hooks/useFetchTeamTableContexts';
+import { FormControl, FormLabel, Select, Skeleton } from '@kvib/react';
+import { useFetchAllContexts } from '../../hooks/useFetchAllContexts';
+import { Context } from '../../hooks/useFetchTeamContexts';
 
 export function CopyContextDropdown({
   tableId,
-  teamId,
   copyContext,
   setCopyContext,
 }: {
   tableId: string;
-  teamId: string;
   copyContext: string | null;
   setCopyContext: (context: string) => void;
 }) {
   const { data: contexts, isLoading: contextsIsLoading } =
-    useFetchTeamTableContexts(teamId, tableId);
+    useFetchAllContexts();
+
+  const contextsForTable: Context[] =
+    contexts?.filter((context) => context.tableId === tableId) ?? [];
+
+  if (!contexts || contexts.length === 0) {
+    return null;
+  }
   return (
-    <Box marginBottom="1rem">
-      <FormLabel htmlFor="select">
+    <FormControl>
+      <FormLabel
+        style={{
+          fontSize: 'small',
+          fontWeight: 'bold',
+        }}
+      >
         Kopier svar fra eksisterende skjema
       </FormLabel>
-      <FormControl>
-        <Skeleton isLoaded={!contextsIsLoading}>
-          <Select
-            id="select"
-            placeholder="Velg skjema"
-            onChange={(e) => setCopyContext(e.target.value)}
-            bgColor="white"
-            borderColor="gray.200"
-            value={copyContext ?? undefined}
-          >
-            {contexts?.map((context) => (
-              <option key={context.id} value={context.id}>
-                {context.name}
-              </option>
-            ))}
-          </Select>
-        </Skeleton>
-      </FormControl>
-    </Box>
+      <Skeleton isLoaded={!contextsIsLoading}>
+        <Select
+          id="select"
+          placeholder="Velg skjema"
+          onChange={(e) => setCopyContext(e.target.value)}
+          bgColor="white"
+          borderColor="gray.200"
+          value={copyContext ?? undefined}
+        >
+          {contextsForTable?.map((context) => (
+            <option key={context.id} value={context.id}>
+              {context.name}
+            </option>
+          ))}
+        </Select>
+      </Skeleton>
+    </FormControl>
   );
 }
