e9404e4e1b91c5389c02b9a7cd26b8036ee2783d
Gyda Elisa Sæter <gyda@bekk-mac-3335.shared>
2025-01-13 16:06:42 +0100

    tableproviders made configurable

--- backend/src/main/kotlin/no/bekk/services/TableService.kt
+++ backend/src/main/kotlin/no/bekk/services/TableService.kt
@@ -1,16 +1,20 @@
 package no.bekk.services
 
+import io.ktor.client.*
+import no.bekk.configuration.AirTableInstanceConfig
 import no.bekk.configuration.AppConfig
+import no.bekk.configuration.YAMLInstanceConfig
 import no.bekk.providers.AirTableProvider
 import no.bekk.providers.TableProvider
+import no.bekk.providers.YamlProvider
 import no.bekk.providers.clients.AirTableClient
 
 object TableService {
 
 
   private val providers: List<TableProvider> = AppConfig.tables.tables.map { table ->
-        if (table.type == "AIRTABLE") {
-            AirTableProvider(
+        when (table) {
+            is AirTableInstanceConfig -> AirTableProvider(
                 id = table.id,
                 airtableClient = AirTableClient(
                     table.accessToken
@@ -21,47 +25,15 @@ object TableService {
                 webhookId = table.webhookId,
                 webhookSecret = table.webhookSecret,
             )
-        } else {
-            throw Exception("Table ${table.type} not found")
+            is YAMLInstanceConfig -> YamlProvider(
+                id = table.id,
+                endpoint = table.endpoint,
+                resourcePath =   table.resourcePath,
+            )
+            else ->  throw Exception("Valid tabletype not found")
+
         }
     }
-
-
-//    private val providers: List<TableProvider> = listOf<TableProvider>(
-//        AirTableProvider(
-//            id = "570e9285-3228-4396-b82b-e9752e23cd73",
-//            airtableClient = AirTableClient(
-//                AppConfig.tables.sikkerhetskontroller.accessToken
-//            ),
-//            baseId = AppConfig.tables.sikkerhetskontroller.baseId,
-//            tableId = AppConfig.tables.sikkerhetskontroller.tableId,
-//            viewId = AppConfig.tables.sikkerhetskontroller.viewId,
-//            webhookId = AppConfig.tables.sikkerhetskontroller.webhookId,
-//            webhookSecret = AppConfig.tables.sikkerhetskontroller.webhookSecret,
-//        ),
-//        AirTableProvider(
-//            id = "816cc808-9188-44a9-8f4b-5642fc2932c4",
-//            airtableClient = AirTableClient(
-//                AppConfig.tables.driftskontinuitet.accessToken
-//            ),
-//            baseId = AppConfig.tables.driftskontinuitet.baseId,
-//            tableId = AppConfig.tables.driftskontinuitet.tableId,
-//            viewId = AppConfig.tables.driftskontinuitet.viewId,
-//            webhookId = AppConfig.tables.driftskontinuitet.webhookId,
-//            webhookSecret = AppConfig.tables.driftskontinuitet.webhookSecret,
-//        ),
-//        AirTableProvider(
-//            id = "test-table",
-//            airtableClient = AirTableClient(
-//                System.getenv("AIRTABLE_TEST_TOKEN")
-//            ),
-//            baseId = "appJFzRzW8GmaKuz3",
-//            tableId = "tblbiZLh6qn3k7wdt",
-//            viewId = "",
-//            webhookId = "achpY1AcPW93B9ucr",
-//            webhookSecret = System.getenv("AIRTABLE_WEBHOOK_SECRET")
-//        ),
- //   )
 
 
     fun getTableProvider(tableId: String): TableProvider {
