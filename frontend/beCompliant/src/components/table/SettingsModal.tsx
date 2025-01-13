88a79dc7819b320acea3dea19389f9e921bdfa0b
starheim98 <andreas.hernas@bekk.no>
2025-01-13 15:13:44 +0100

    copy context answers in settings menu

--- frontend/beCompliant/src/components/table/SettingsModal.tsx
+++ frontend/beCompliant/src/components/table/SettingsModal.tsx
@@ -10,6 +10,7 @@ import {
   ModalHeader,
   ModalOverlay,
   Select,
+  Skeleton,
   Stack,
   useToast,
 } from '@kvib/react';
@@ -18,6 +19,8 @@ import { useFetchContext } from '../../hooks/useFetchContext';
 import { useParams } from 'react-router-dom';
 import { apiConfig } from '../../api/apiConfig';
 import { axiosFetch } from '../../api/Fetch';
+import { useFetchAllContexts } from '../../hooks/useFetchAllContexts';
+import { Context } from '../../hooks/useFetchTeamContexts';
 
 type Props = {
   onOpen: () => void;
@@ -32,44 +35,83 @@ export function SettingsModal({ onClose, isOpen }: Props) {
   const { data: userinfo } = useFetchUserinfo();
   const currentContext = useFetchContext(contextId);
 
-  const handleChangeContextTeam = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
+  const { data: contexts, isPending: contextsIsLoading } =
+    useFetchAllContexts();
+
+  const handleSettingsSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!contextId) return;
     const form = e.target as HTMLFormElement;
     const changeTeamElement = form.elements.namedItem(
-      'edit_team'
+      'editTeam'
     ) as HTMLSelectElement;
-    if (!changeTeamElement?.value) {
-      return;
-    }
-    const newTeamId = changeTeamElement.value;
-    try {
-      const response = await axiosFetch({
-        url: apiConfig.contexts.forIdAndTeam.url(contextId),
-        method: 'PATCH',
-        data: {
-          teamId: newTeamId,
-        },
-      });
-      if (response.status === 200 || response.status === 204) {
-        onClose();
-      }
-    } catch (error) {
-      const toastId = 'change-context-team-error';
-      if (!toast.isActive(toastId)) {
-        toast({
-          id: toastId,
-          title: 'Å nei!',
-          description: 'Det har skjedd en feil. Prøv på nytt',
-          status: 'error',
-          duration: 5000,
-          isClosable: true,
+    const copyContextElement = form.elements.namedItem(
+      'copyContextSelect'
+    ) as HTMLSelectElement;
+
+    if (changeTeamElement?.value) {
+      const newTeamId = changeTeamElement.value;
+      try {
+        const response = await axiosFetch({
+          url: apiConfig.contexts.forIdAndTeam.url(contextId) + '/team',
+          method: 'PATCH',
+          data: {
+            teamId: newTeamId,
+          },
         });
+        if (response.status === 200 || response.status === 204) {
+          onClose();
+        }
+      } catch (error) {
+        const toastId = 'change-context-team-error';
+        if (!toast.isActive(toastId)) {
+          toast({
+            id: toastId,
+            title: 'Å nei!',
+            description: 'Det har skjedd en feil. Prøv på nytt',
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+          });
+        }
+      }
+    }
+    if (copyContextElement?.value) {
+      try {
+        const response = await axiosFetch({
+          url: apiConfig.contexts.byId.url(contextId) + '/answers',
+          method: 'PATCH',
+          data: {
+            copyContextId: copyContextElement.value,
+          },
+        });
+        if (response.status === 200 || response.status === 204) {
+          onClose();
+        }
+      } catch (error) {
+        const toastId = 'copy-context-error';
+        if (!toast.isActive(toastId)) {
+          toast({
+            id: toastId,
+            title: 'Å nei!',
+            description: 'Det har skjedd en feil. Prøv på nytt',
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+          });
+        }
       }
     }
   };
+
+  const contextsForTable: Context[] =
+    contexts?.filter(
+      (context) =>
+        context.tableId === currentContext.data?.tableId &&
+        context.id !== currentContext.data?.id
+    ) ?? [];
+
+  const isDisabled = contextsForTable.length === 0;
 
   return (
     <Modal onClose={onClose} isOpen={isOpen} isCentered>
@@ -77,13 +119,13 @@ export function SettingsModal({ onClose, isOpen }: Props) {
       <ModalContent>
         <ModalHeader>Endre skjemautfylling</ModalHeader>
         <ModalBody>
-          <form onSubmit={handleChangeContextTeam}>
+          <form onSubmit={handleSettingsSubmit}>
             <Stack gap="1rem">
               <FormControl>
                 <FormLabel>
                   Endre teamet dette skjemaet skal gjelde for:
                 </FormLabel>
-                <Select name="edit_team" placeholder="Velg team">
+                <Select name="editTeam" placeholder="Velg team">
                   {userinfo?.groups.map((team) => {
                     if (team.id === currentContext.data?.teamId) {
                       return null;
@@ -95,6 +137,27 @@ export function SettingsModal({ onClose, isOpen }: Props) {
                     );
                   })}
                 </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Kopier svar fra et annet skjema:</FormLabel>
+                <Skeleton isLoaded={!contextsIsLoading}>
+                  <Select
+                    name="copyContextSelect"
+                    placeholder={
+                      isDisabled
+                        ? 'Ingen eksisterende skjema funnet'
+                        : 'Velg skjema'
+                    }
+                    bgColor="white"
+                    borderColor="gray.200"
+                  >
+                    {contextsForTable?.map((context) => (
+                      <option key={context.id} value={context.id}>
+                        {context.name}
+                      </option>
+                    ))}
+                  </Select>
+                </Skeleton>
               </FormControl>
 
               <HStack justifyContent="end">
