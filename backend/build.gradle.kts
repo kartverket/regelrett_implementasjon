fca91c71d400324cf8d2f876dd2e39d8fb7d1513
JegHeterGyda <100707649+JegHeterGyda@users.noreply.github.com>
2025-02-28 09:22:36 +0100

    631 fix sårbarheter (#632)
    * oppgraderte msal fra .16 til .19

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -51,7 +51,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     implementation("net.mamoe.yamlkt:yamlkt:0.13.0")    // This is the same library that ktor uses
     implementation("org.jetbrains.exposed:exposed-core:0.36.1")
-    implementation("com.microsoft.azure:msal4j:1.16.0")
+    implementation("com.microsoft.azure:msal4j:1.19.0")
+    implementation("net.minidev:json-smart:2.5.2") // Kan slettes når msal ograderer json smart til 5.2.
     implementation("org.jetbrains.exposed:exposed-dao:0.36.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
     implementation("org.jooq:jooq:3.19.8")
