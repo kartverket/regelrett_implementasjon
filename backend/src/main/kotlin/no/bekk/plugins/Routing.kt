325764d5398ca3b4487e10e494e71703d4d87f2f
malinnsnieske <malin.snieske@bekk.no>
2025-03-03 15:47:59 +0100

    remove unused schema migration endpoint (#641)
    Co-authored-by: Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>

--- backend/src/main/kotlin/no/bekk/plugins/Routing.kt
+++ backend/src/main/kotlin/no/bekk/plugins/Routing.kt
@@ -3,13 +3,10 @@ package no.bekk.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import no.bekk.database.ContextRepository
 import no.bekk.routes.*
 import no.bekk.services.FormService
-import no.bekk.util.logger
 
 fun Application.configureRouting() {
 
@@ -27,15 +24,6 @@ fun Application.configureRouting() {
                 it.getSchema()
             }
             call.respond(schemas)
-        }
-
-        //This endpoint can be removed after schemas in frisk are migrated to new metadata
-        get("/contexts/{contextId}/tableId") {
-            logger.debug("Received GET /contexts/{contextId}/tableId with id: ${call.parameters["contextId"]}")
-            val contextId = call.parameters["contextId"] ?: throw BadRequestException("Missing contextId")
-            val tableId = ContextRepository.getContextTableId(contextId);
-            call.respond(HttpStatusCode.OK, tableId)
-            return@get
         }
 
         authenticate("auth-jwt") {
